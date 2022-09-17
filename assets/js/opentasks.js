5b67e1c437ab4c5dafecc874628aaa3b290149f7
Breno Ribeiro <brenoribeiro.dev@gmail.com>
2022-09-17 01:20:54 -0300

    update

--- assets/js/opentasks.js
+++ assets/js/opentasks.js
@@ -1,14 +1,14 @@
 const button = document.querySelector('.btn_tasks');
 const tasks = document.querySelector('.next_updates');
 
-button.innerText = "Tarefas pendente";
+button.innerText = "Tarefas pendentes";
 
 button.addEventListener('click', function(){
     tasks.classList.toggle('taskActive');
     button.innerText = 'Fechar';
     button.classList.toggle('active');
         if(tasks.classList == 'next_updates'){
-            button.innerText = 'Tarefas pendente';
+            button.innerText = 'Tarefas pendentes';
         }
     
 })
